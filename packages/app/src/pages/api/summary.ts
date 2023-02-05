74a296ec341faceec1613625da25c047ddb98cf9
xcv58 <i@xcv58.com>
2023-02-05 13:24:30 -0500

    Add cors support

--- packages/app/src/pages/api/summary.ts
+++ packages/app/src/pages/api/summary.ts
@@ -1,6 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Configuration, OpenAIApi } from "openai";
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['POST', 'HEAD'],
+})
+
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
+}
 
 type Data = {
   data?: string;
@@ -17,6 +40,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await runMiddleware(req, res, cors)
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
