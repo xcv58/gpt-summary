37ed56cf99051267ecfa7e112f4cde259e251f5c
xcv58 <i@xcv58.com>
2023-02-05 16:09:55 -0500

    Trim result

--- packages/app/src/pages/api/summary.ts
+++ packages/app/src/pages/api/summary.ts
@@ -1,12 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Configuration, OpenAIApi } from "openai";
-import Cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai'
+import Cors from 'cors'
 
 const cors = Cors({
   methods: ['POST', 'HEAD'],
 })
-
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
@@ -26,15 +25,15 @@ function runMiddleware(
 }
 
 type Data = {
-  data?: string;
-  error?: { message: string };
+  data?: string
+  error?: { message: string }
 }
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
+})
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration)
 
 export default async function handler(
   req: NextApiRequest,
@@ -44,51 +43,52 @@ export default async function handler(
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
-    return;
+        message:
+          'OpenAI API key not configured, please follow instructions in README.md',
+      },
+    })
+    return
   }
 
-  const content = req.body.content || '';
+  const content = req.body.content || ''
   if (content.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid content",
-      }
-    });
-    return;
+        message: 'Please enter a valid content',
+      },
+    })
+    return
   }
 
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: 'text-davinci-003',
       prompt: generatePrompt(content),
       temperature: 0.0,
       max_tokens: 256,
       presence_penalty: 1,
       frequency_penalty: 1,
-    });
+    })
     const result = completion.data.choices[0].text
-    res.status(200).json({ data: result });
-  } catch(error: any) {
+    res.status(200).json({ data: result?.trim() })
+  } catch (error: any) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
+      console.error(error.response.status, error.response.data)
+      res.status(error.response.status).json(error.response.data)
     } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
+      console.error(`Error with OpenAI API request: ${error.message}`)
       res.status(500).json({
         error: {
           message: 'An error occurred during your request.',
-        }
-      });
+        },
+      })
     }
   }
 }
 
 function generatePrompt(content: string) {
   return `${content}
-write summary for above content:`;
-// Tl;dr`;
+write summary for above content:`
+  // Tl;dr`;
 }
