0440878edddb8e8fb64e8fe1375c441fcd003ec1
xcv58 <i@xcv58.com>
2023-02-05 13:42:10 -0500

    Add prettier support

--- packages/extension/background.js
+++ packages/extension/background.js
@@ -1,23 +1,23 @@
 const LOADING = ' · · · '
 
 function getSelectionOrContent() {
-  const selection = (window.getSelection().toString() || '').trim();
+  const selection = (window.getSelection().toString() || '').trim()
   if (selection) {
     return selection
   }
-  return window.document.body.innerText;
+  return window.document.body.innerText
 }
 
 async function fetchSummary(content) {
   try {
-    response = await fetch("https://gpt-summary.xcv58.org/api/summary", {
-      method: "POST",
+    response = await fetch('https://gpt-summary.xcv58.org/api/summary', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ content })
+      body: JSON.stringify({ content }),
     })
-    const data = await response.json();
+    const data = await response.json()
     return data
   } catch (e) {
     console.error(error)
@@ -26,45 +26,45 @@ async function fetchSummary(content) {
 }
 
 chrome.action.onClicked.addListener(async (tab) => {
-  console.log('onclick');
+  console.log('onclick')
   const tabId = tab.id
   const prevState = await chrome.action.getBadgeText({
-    tabId
-  });
+    tabId,
+  })
   const isLoading = prevState === LOADING
   if (!isLoading) {
     await chrome.action.setBadgeText({
       tabId,
       text: LOADING,
-    });
+    })
   } else {
-    console.log('Pending request');
+    console.log('Pending request')
     return
   }
   let results = await chrome.scripting.executeScript({
     target: { tabId },
     func: getSelectionOrContent,
   })
-  console.log(results);
+  console.log(results)
   if (!results || !Array.isArray(results) || results.length <= 0) {
-    console.log('no results: ', results);
+    console.log('no results: ', results)
     return
   }
   const [res] = results
   const { result } = res
-  console.log(result);
+  console.log(result)
 
   const { data, error } = await fetchSummary(result)
-  console.log({ data, error });
+  console.log({ data, error })
   await chrome.action.setBadgeText({
     tabId,
-    text: ''
-  });
+    text: '',
+  })
   await chrome.scripting.executeScript({
     target: { tabId },
     func: (data) => {
       console.log('data:', data)
     },
-    args: [data]
+    args: [data],
   })
-});
+})
