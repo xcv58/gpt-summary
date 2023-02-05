98da3a28883db0330a79847ce9187a6a581721c9
xcv58 <i@xcv58.com>
2023-02-04 19:45:54 -0500

    Add /api/summary endpoint

---
+++ src/pages/api/summary.ts
@@ -0,0 +1,70 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from "openai";
+
+type Data = {
+  data?: string;
+  error?: { message: string };
+}
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const content = req.body.content || '';
+  if (content.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid content",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(content),
+      temperature: 0.0,
+      max_tokens: 256,
+      presence_penalty: 1,
+      frequency_penalty: 1,
+    });
+    const result = completion.data.choices[0].text
+    res.status(200).json({ data: result });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(content: string) {
+  return `${content}
+write summary for above content:`;
+// Tl;dr`;
+}
