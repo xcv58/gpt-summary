05485dcc72b73c83ed3baae0572861449b6b1aa9
xcv58 <i@xcv58.com>
2023-02-05 16:24:25 -0500

    Add modal support

--- packages/extension/background.js
+++ packages/extension/background.js
@@ -25,46 +25,94 @@ async function fetchSummary(content) {
   }
 }
 
-chrome.action.onClicked.addListener(async (tab) => {
-  console.log('onclick')
-  const tabId = tab.id
-  const prevState = await chrome.action.getBadgeText({
-    tabId,
-  })
-  const isLoading = prevState === LOADING
-  if (!isLoading) {
-    await chrome.action.setBadgeText({
-      tabId,
-      text: LOADING,
-    })
-  } else {
-    console.log('Pending request')
-    return
-  }
-  let results = await chrome.scripting.executeScript({
-    target: { tabId },
-    func: getSelectionOrContent,
-  })
-  console.log(results)
+async function executeScript(options) {
+  let results = await chrome.scripting.executeScript(options)
   if (!results || !Array.isArray(results) || results.length <= 0) {
     console.log('no results: ', results)
     return
   }
-  const [res] = results
-  const { result } = res
-  console.log(result)
+  return results[0]
+}
 
-  const { data, error } = await fetchSummary(result)
-  console.log({ data, error })
+async function upsertInjections(tabId) {
+  const modalInjected = await executeScript({
+    target: { tabId },
+    func: () => {
+      return (
+        window.document.querySelector('#gpt-summary-modal') && window.MicroModal
+      )
+    },
+  })
+  if (!modalInjected.result) {
+    await chrome.scripting.insertCSS({
+      target: { tabId },
+      files: ['modal.css'],
+    })
+    return await chrome.scripting.executeScript({
+      target: { tabId },
+      files: ['topbar.min.js', 'micromodal.min.js', 'modal.js'],
+    })
+  }
+}
+
+async function sendResult(tabId, data) {
   await chrome.action.setBadgeText({
     tabId,
     text: '',
   })
   await chrome.scripting.executeScript({
     target: { tabId },
+    func: () => window.topbar.hide(),
+  })
+  await chrome.scripting.executeScript({
+    target: { tabId },
     func: (data) => {
       console.log('data:', data)
+      const contentDiv = window.document.querySelector(
+        '#gpt-summary-modal-content'
+      )
+      contentDiv.innerText = data
+      const MicroModal = window.MicroModal
+      MicroModal.init()
+      MicroModal.show('gpt-summary-modal')
     },
     args: [data],
   })
+}
+
+async function setLoadingIndicator(tabId) {
+  await chrome.action.setBadgeText({
+    tabId,
+    text: LOADING,
+  })
+  await chrome.scripting.executeScript({
+    target: { tabId },
+    func: () => window.topbar.show(),
+  })
+}
+
+chrome.action.onClicked.addListener(async (tab) => {
+  console.log('onclick')
+  const tabId = tab.id
+  await upsertInjections(tabId)
+  const prevState = await chrome.action.getBadgeText({
+    tabId,
+  })
+  const isLoading = prevState === LOADING
+  if (!isLoading) {
+    await setLoadingIndicator(tabId)
+  } else {
+    console.log('Pending request')
+    return
+  }
+
+  const { result } = await executeScript({
+    target: { tabId },
+    func: getSelectionOrContent,
+  })
+  console.log(result)
+
+  const { data, error } = await fetchSummary(result)
+  console.log({ data, error })
+  await sendResult(tabId, data)
 })
