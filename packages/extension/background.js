3f41dd31b312a1b98bf8f0e39bea9b99d365725a
xcv58 <i@xcv58.com>
2023-02-05 19:11:25 -0500

    Update the modal position to top right

--- packages/extension/background.js
+++ packages/extension/background.js
@@ -9,6 +9,24 @@ function getSelectionOrContent() {
 }
 
 async function fetchSummary(content) {
+  await new Promise((r) => setTimeout(r, 100))
+  return {
+    data: `
+    We design, build, and evaluate novel computer systems. We are currently working on smartphones, today's most pervasive mobile technology. Our research aims to make smartphones more useful, safer, and more sustainable. But our primary goal is training computer scientists and our primary product is people. We try to align everything in service of that goal, which results in putting people before projects before papers.
+    People
+    Putting people first. Our primary job is training future researchers and developers. Lab members are here to learn. Students come up with new ideas, develop prototype systems, evaluate them, and then communicate their results to the broader scientific community. Along the way they are developing analytical and leadership abilities to bootstrap a successful career in technology.
+    Projects
+    Putting people before projects. Group members choose and lead projects that excite them. Projects proceed at a pace that benefits members' personal development. Along the way, we are careful not to miss opportunities to allow group members to learn and grow. Sometimes this means development doesn't always happen as fast as possible.
+    Papers
+    Putting projects before papers. We work on interesting projects with potential for impact—not just ones that might quickly produce publications. When we discover something interesting, publishing is our chance to share it with the world. We don't work on projects to publish papers—we work on projects because we're curious about what happens when we try something new.
+    Together In the Lab
+    PhoneLab
+    blue is fortunate to have a single lab large enough to house all current group members. We work in Davis Hall Room 301B, also know as the PhoneLab. All group members—faculty, administrators, graduate students and undergraduates—work side-by-side in our lab. This arrangement is inspired by the organization of groups like the Berkeley AMP Lab, as well as the organization of typical startups and technology companies.
+
+    Working together allows for spontaneous collaboration and problem solving. Group members are asked to be present in the lab during certain hours to facilitate interaction. New members find it easy to get started since there are always more experienced developers nearby. Many group discussions that take place in the lab have benefited from being overheard by group members that were not directly engaged in the project. It also just ends up being a lot of fun!
+
+  `,
+  }
   try {
     response = await fetch('https://gpt-summary.xcv58.org/api/summary', {
       method: 'POST',
@@ -73,6 +91,7 @@ async function sendResult(tabId, data) {
       )
       contentDiv.innerText = data
       const MicroModal = window.MicroModal
+      console.log({ MicroModal }, MicroModal.config)
       MicroModal.init()
       MicroModal.show('gpt-summary-modal')
     },
--- packages/extension/modal.js
+++ packages/extension/modal.js
@@ -2,23 +2,22 @@ function createModal() {
   const newDiv = document.createElement('div')
   newDiv.innerHTML = `
   <div class="modal micromodal-slide" id="gpt-summary-modal" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+    <div class="modal__overlay" tabindex="-1">
       <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="gpt-summary-modal-title">
-        <header class="modal__header">
+        <div class="modal__header">
           <h2 class="modal__title" id="gpt-summary-modal-title">
-            Micromodal
+            GPT Summary
           </h2>
           <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
-        </header>
-        <main class="modal__content" id="gpt-summary-modal-content">
+        </div>
+        <div class="modal__content" id="gpt-summary-modal-content">
           <p>
             Try hitting the <code>tab</code> key and notice how the focus stays within the modal itself. Also, <code>esc</code> to close modal.
           </p>
-        </main>
-        <footer class="modal__footer">
+        </div>
+        <div class="modal__footer">
           <button class="modal__btn modal__btn-primary">Continue</button>
-          <button class="modal__btn" data-micromodal-close aria-label="Close this dialog window">Close</button>
-        </footer>
+        </div>
       </div>
     </div>
   </div>
